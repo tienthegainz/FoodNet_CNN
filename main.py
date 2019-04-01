5c0ea8a53dff4f4aaca0c31b4fb447134babd143
Tien <viettienha98@gmail.com>
2019-04-01 23:38:00 +0700

    Add .h5 as trained model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from custom_CNN.custom_cnn import build_custom_CNN
 from process_images.process import load_class, load_image
+from keras.callbacks import ModelCheckpoint
 import h5py
 from pathlib import Path
 
@@ -15,25 +16,28 @@ if __name__ == '__main__':
     if check_point.is_file():
         print('Load model\n')
         model = load_model('check_point.h5')
+        scores = model.evaluate(
+            X_test,
+            Y_test,
+            verbose=1
+        )
+        print('\nAccuracy: %.2f%%' % (scores[1] * 100))
     else:
+        # Init
         print('Init model\n')
         model = build_custom_CNN(len(classes))
+        # Training
+        filepath = "check_point.h5"
+        checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+        callbacks_list = [checkpoint]
+        model.fit(
+            X_tr,
+            Y_tr,
+            validation_data=(X_val, Y_val),
+            epochs = 200,
+            batch_size = 100,
+            verbose=1,
+            callbacks=callbacks_list
+        )
 
-    filepath = "check_point.h5"
-    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-    callbacks_list = [checkpoint]
-    model.fit(
-        X_tr,
-        Y_tr,
-        validation_data=(X_val, Y_val),
-        epochs = 200,
-        batch_size = 100,
-        verbose=1,
-        callbacks=callbacks_list
-    )
-    scores = model.evaluate(
-        X_test,
-        Y_test,
-        verbose=1
-    )
     # model.save('check_point.h5')
